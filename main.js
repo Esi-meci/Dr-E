8d4dbf696386cf70fffedb81d3a0efc8a71d1f75
Esi Meci <gowinmercy@gmail.com>
2025-08-08 13:43:18 +0100

    adding Calendly feature

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
 function toggle_theme() {
-
-    const toggleButton = document.querySelector('.theme');
-    // alert('clicked')
-    document.body.classList.toggle('dark-theme');
-
+  const toggleButton = document.querySelector(".theme");
+  // alert('clicked')
+  document.body.classList.toggle("dark-theme");
 }
 
-
+const openBtn = document.getElementById("openModalBtn");
 const modal = document.getElementById("bookingModal");
-const openBtn = document.querySelector(".open-modal-btn");
-const closeBtn = document.getElementById("closeModal");
+const closeBtn = document.getElementById("closeModalBtn");
 
+// Open modal
 openBtn.addEventListener("click", () => {
-  modal.style.display = "block";
+  modal.style.display = "flex";
 });
 
+// Close modal
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-// Close modal if clicking outside the modal content
+// Close when clicking outside modal content
 window.addEventListener("click", (e) => {
-  if (e.target == modal) {
+  if (e.target === modal) {
     modal.style.display = "none";
   }
 });
+
+window.onload = function () {
+  const bookBtn = document.getElementById("bookBtn");
+
+  bookBtn.addEventListener("click", function () {
+    if (window.Calendly) {
+      Calendly.initPopupWidget({
+        url: 'https://calendly.com/cherrychiro8'
+      });
+      console.log('calendly is here')
+    } else {
+      console.error('Calendly script not loaded yet.');
+    }
+    return false;
+  });
+};
