1235c1511bc236941d4c3948104169be93fd572c
Esi Meci <gowinmercy@gmail.com>
2025-08-08 10:41:11 +0100

    Adding functionality

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ function toggle_theme() {
     document.body.classList.toggle('dark-theme');
 
 }
+
+
+const modal = document.getElementById("bookingModal");
+const openBtn = document.querySelector(".open-modal-btn");
+const closeBtn = document.getElementById("closeModal");
+
+openBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Close modal if clicking outside the modal content
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
